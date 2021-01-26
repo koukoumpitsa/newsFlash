d97998090d4119717eec9bcf6be7ad0ec01a1c51
Dimitra Chantzopoulou <d.chantzopoulou@gmail.com>
2021-01-26 15:32:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, { useEffect } from "react";
 import "./styles.css";
+import useFetch from "./useFetch";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  const { get, isLoading } = useFetch("https://newsapi.org/");
+
+  useEffect(() => {
+    get("v2/top-headlines?country=us&apiKey=f6c6deeaa5d14d9093099b927cea2961")
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return null;
 }
---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const NavBar = styled.div``;
+
+export default function Navbar() {
+  return <NavBar>blach blach</NavBar>;
+}
+
+//key: f6c6deeaa5d14d9093099b927cea2961
