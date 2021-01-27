66d7cd4e9ff27f37d8292e1e4dc23a9890fd87f6
Dimitra Chantzopoulou <d.chantzopoulou@gmail.com>
2021-01-27 08:19:20 +0100

    initial check on select-fetch feauture

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React, { useEffect } from "react";
-import Navbar from "./Navbar";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import useFetch from "./useFetch";
 
 export default function App() {
+  const [topic, setTopic] = useState("");
+  const [content, setContent] = useState("");
   const { get, isLoading } = useFetch(
     "https://api.nytimes.com/svc/topstories/v2/"
   );
 
   useEffect(() => {
-    get("home.json?api-key=M2MJ1tu97KN0b4aNn7xVudoO7owl2jgA")
-      .then((data) => console.log(data))
+    get(`${topic}.json?api-key=M2MJ1tu97KN0b4aNn7xVudoO7owl2jgA`)
+      .then((data) => {
+        console.log(data);
+        setContent(data.num_results);
+      })
       .catch((error) => console.log(error));
-  }, []);
+  }, [topic]);
 
-  return <Navbar />;
+  return (
+    <>
+      <h3>Change of topic</h3>
+      <select onChange={(e) => setTopic(e.target.value)}>
+        <option value="home">General News</option>
+        <option value="arts">Arts</option>
+        <option value="automobiles">automobiles</option>
+        <option value="books">Books</option>
+      </select>
+
+      <h4>hello there are {content} articles about this topic </h4>
+    </>
+  );
 }
 
 //M2MJ1tu97KN0b4aNn7xVudoO7owl2jgA
